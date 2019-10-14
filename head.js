5dce769b7fbc2e8568738188861032a29b928680
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-14 21:48:57 +0000

    added head to assertEqual function

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function(array) {
+let output = array[0]; //return first item in array
+
+return output;
+};
+
+//console.log(head([1, 2, 3]));
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+    
+  };
+  
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
