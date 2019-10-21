9f7cf86eb3bd322fce45d5a4f54b31f702097b07
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-21 16:59:15 +0000

    refactored tail code

--- tail.js
+++ tail.js
@@ -5,16 +5,6 @@ const tail = function(array) {
   return output;
 };
 
-// console.log(tail([1, 2, 3]));
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
 // TEST CODE
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
