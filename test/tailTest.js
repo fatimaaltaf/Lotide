c2a7e3897cb4f0450a875af4c78ca4071a3f60b7
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-21 19:42:40 +0000

    refactored tailTest with chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,12 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
+describe("#tail#", () => {
+    it("returns  for [Yo Yo, Lighthouse, Labs]", () => {
+        assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+    });
+});
 
 // TEST CODE
 const words = ["Yo Yo", "Lighthouse", "Labs"];
